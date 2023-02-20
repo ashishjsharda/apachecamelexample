97634da1f9806cf310ccac1d1309836bc2e18fbc
Ashish Sharda <ashishjsharda@gmail.com>
2023-02-19 18:34:04 -0600

    first commit

---
+++ src/test/java/com/example/apachecameldemo/ApachecameldemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apachecameldemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApachecameldemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
