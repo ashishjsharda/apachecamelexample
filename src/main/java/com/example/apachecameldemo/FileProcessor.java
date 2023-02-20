755d9f235f405583115b5ed9891d2e58d69c74ff
Ashish Sharda <ashishjsharda@gmail.com>
2023-02-19 18:41:15 -0600

    implement process

--- src/main/java/com/example/apachecameldemo/FileProcessor.java
+++ src/main/java/com/example/apachecameldemo/FileProcessor.java
@@ -1,15 +1,17 @@
 package com.example.apachecameldemo;
-
 import org.apache.camel.Processor;
 
+/**
+ * Add logic to modify name
+ * @author ashish
+ */
 public class FileProcessor implements Processor {
 
         @Override
         public void process(org.apache.camel.Exchange exchange) throws Exception {
-
-                System.out.println("FileProcessor.process()");
-
+               String originalFile = exchange.getIn().getBody(String.class);
+               String modifiedFile = originalFile.replaceAll("Apache", "Red Hat");
+                exchange.getIn().setBody(modifiedFile);
         }
-
 }
 
